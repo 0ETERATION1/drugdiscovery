114cb5d8d7ee64c40afcfe6b45079930c55d48c9
Alex Terskin <terskinalex@gmail.com>
2024-10-01 00:09:07 -0400

    in molecule page moved text input arrow to point down, which shows where output will go

--- components/MoleculeAI.tsx
+++ components/MoleculeAI.tsx
@@ -131,7 +131,7 @@ const MoleculeAI: React.FC<MoleculeAIProps> = ({ molData, diseaseName }) => {
                   strokeLinecap="round"
                   strokeLinejoin="round"
                   strokeWidth={2}
-                  d="M5 10l7-7m0 0l7 7m-7-7v18"
+                  d="M19 14l-7 7m0 0l-7-7m7 7V3"
                 />
               </svg>
             </button>
